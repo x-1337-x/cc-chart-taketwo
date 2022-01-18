4d90f1552b76912e3fb9c7689aa0a19fc3f12922
x-1337-x <g1thub_acc@protonmail.com>
2022-01-18 16:40:12 +0200

    index clean-up

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-// import fastify from 'fastify';
-
-// const server = fastify();
-
-// server.get('/ping', async (request, reply) => {
-// 	return 'pong\n';
-// });
-
-// server.listen(8080, (err, address) => {
-// 	if (err) {
-// 		console.error(err);
-// 		process.exit(1);
-// 	}
-// 	console.log(`Server listening at ${address}`);
-// });
-
 import { AddressInfo } from 'net';
 
 import { PORT } from './constants';
@@ -25,7 +9,6 @@ setupDB('server').then(async () => {
 	try {
 		await server.listen(PORT, '0.0.0.0');
 		server.blipp();
-		// console.log(`server listening on http://localhost:${PORT}`);
 		console.log(
 			`server listening on ${(server.server.address() as AddressInfo).port}`
 		);
@@ -42,5 +25,3 @@ process.on('uncaughtException', (error) => {
 process.on('unhandledRejection', (error) => {
 	console.error(error);
 });
-
-// start();
